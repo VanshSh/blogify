bf6c61a29c194ece9c6d49e17b6dd858f930a21f
Vansh Sharma <vansh@nowandme.com>
2022-07-02 00:09:20 +0530

    feat: :sparkles: Add Post card style

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import { PostCard, PostWidget, Categories } from '../components'
+import { getPosts } from '../services'
+
+const Home = ({ posts }) => {
+    return (
+        <div className='container mx-auto px-10 mb-10 '>
+            <Head>
+                <title>Blogify</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+                <div className='lg:col-span-8 col-span-1'>
+                    {posts.map((post) => {
+                        return <PostCard post={post.node} key={post.title} />
+                    })}
+                </div>
+                <div className='lg:col-span-4 col-span-1 '>
+                    <div className='lg:sticky relative top-8'>
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const posts = (await getPosts()) || []
+
+    return {
+        props: { posts },
+    }
+}
+
+export default Home
